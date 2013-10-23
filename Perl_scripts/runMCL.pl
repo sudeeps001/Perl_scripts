0c4ea628a57d15ac64d80fad8ed032d14143af70
sudeeps-LT <me@fakeServer.com>
2013-10-23 12:38:49 +0200

    modified command line input

--- Perl_scripts/runMCL.pl
+++ Perl_scripts/runMCL.pl
@@ -4,27 +4,83 @@ use warnings;
 use Cwd;
 use Carp;
 use IO::File;
+use Getopt::Long;
 
 #########################################################
 # perl script to run mcl for a given file for inflation values
 # assuming that mcl is installed and available in $PATH
 #########################################################
 
+my $input;
+my $start;
+my $end;
+my $loop;
+# some inputs 
+GetOptions('f=s'=>\$input,'b=f'=>\$start,'e=f'=>\$end,'l=i'=>\$loop);
+unless(defined($input)){
+	help_message();
+}
+unless(defined($start)){
+	$start=1;
+	print "\t parameter -b was not defined, start inflation used: 1 (default)\n"
+}
+unless(defined($end)){
+	$end = 3;
+	print "\t parameter -e was not defined, end inflation used: 3 (default)\n"
+}
+unless(defined($loop)){
+	$loop = 10000;
+	print "\t parameter -l was not defined, number of loops 10000 (default)\n"
+}
+
+# run mcl for a given file for inflation values
+
 my %printHash;
-my $input=$ARGV[0] or croak( "need *.mci as input!\n");
-my $start= $ARGV[1] or croak( "need a starting value (usuallay 1.2)!\n");
-my $end= $ARGV[2] or croak( "need an end value!\n");
+
 my $workDir = &Cwd::cwd();
 
-my $mclOut = $workDir.'/mcl.out';
+my $mciFile="";
 
-runMCL();
+my $mclOut = $workDir.'/'.$input;
+$mclOut=~s/\..*$/\.mcl\.out/gi;
+
+if($input=~/^.*\.mci/gi){
+	runMCL_mci();
+}else{
+	runMCL_abc();
+}
+
 getMCLstat();
 
-sub runMCL{
+################## SUB ROUTINES ###########################################
+
+sub help_message{
+	print "\n\nError! run file mci/abc format not given!\n";
+	print "Call runMCL.pl -f input mci/abc -b begin inflation (optional, default 1) -e end inflation (optional, default 3) -l loop number (optional, default 10000)\n\n";
+	die();
+}
+
+sub runMCL_mci{
 	for(my $i=$start;$i<=$end;$i=$i+0.1){	
 		system("mcl","$input","-I","$i");
 	}
+	$mciFile= $input;
+}
+
+sub runMCL_abc{
+	my $mciFile= $input;
+	my $tabFile = $input;
+	$mciFile=~s/\..*$/\.mci/gi;
+	$tabFile=~s/\..*$/\.tab/gi;
+	system("mcxload","-abc","$input","--stream-mirror",
+	"-write-tab","$tabFile","-o","$mciFile");
+	$input=$mciFile;
+	for(my $i=$start;$i<=$end;$i=$i+0.1){	
+		system("mcl","$input","-I","$i","-l","$loop");
+	}
+#	if running mcl with abc file clm needs .mci for generating statistics
+	
+	
 }
 
 sub getMCLstat{
@@ -38,7 +94,7 @@ sub getMCLstat{
 	foreach my $file(@allFiles){
 		if($file=~/.*\.$input\.I\d+/){
 			$mciInfoInput= $mciInfoInput.' '.$file;	
-			my $input=`/home/sahadeva/bin/mcl-cluster/bin/clm info $input $file`;
+			my $input=`clm info $input $file`;
 #			print $input,"\n";
 			push(@data,$input);
 		}
